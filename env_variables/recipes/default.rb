083833e7d06fc24dea5daef83e0f06efec25edd1
itschn <lofd23@yahoo.de>
2015-05-19 17:12:50 +0200

    Adjusting default

--- env_variables/recipes/default.rb
+++ env_variables/recipes/default.rb
@@ -1,7 +1,9 @@
-application_environment_file do
-  user deploy[:user]
-  group deploy[:group]
+node[:deploy].each do |app_name, deploy|
+  application_environment_file do
+    user deploy[:user]
+    group deploy[:group]
   
-  path ::File.join(deploy[:deploy_to], "shared")
-  environment_variables deploy[:environment_variables]
+    path ::File.join(deploy[:deploy_to], "shared")
+    environment_variables deploy[:environment_variables]
+  end
 end
