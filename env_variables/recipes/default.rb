d959fe20fb1da6274ce67242859388e08ebfc88b
itschn <lofd23@yahoo.de>
2015-05-19 16:31:10 +0200

    Adding env variables

---
+++ env_variables/recipes/default.rb
@@ -0,0 +1,7 @@
+application_environment_file do
+  user deploy[:user]
+  group deploy[:group]
+  
+  path ::File.join(deploy[:deploy_to], "shared")
+  environment_variables deploy[:environment_variables]
+end
