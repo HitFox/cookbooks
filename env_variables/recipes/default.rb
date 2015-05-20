311a70e7c1e67d0d834b482d3ab60b85122141ef
itschn <lofd23@yahoo.de>
2015-05-20 15:46:49 +0200

    Removing symlink

--- env_variables/recipes/default.rb
+++ env_variables/recipes/default.rb
@@ -6,9 +6,4 @@ node[:deploy].each do |app_name, deploy|
     path ::File.join(deploy[:deploy_to], 'shared')
     environment_variables deploy[:environment_variables]
   end
-  
-  # link ::File.join(deploy[:deploy_to], 'shared', 'app.env') do
-  #   to ::File.join(deploy[:deploy_to], 'current', 'app.env')
-  #   link_type :symbolic
-  # end
 end
