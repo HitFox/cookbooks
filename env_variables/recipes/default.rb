ee4ff6e96f0eb8f9f5c898f822684aa9a6ed5e2d
Michael Rüffer <michaelrueffer@Michaels-MacBook.local>
2015-05-20 14:45:58 +0200

    Adding symbolic link for app.env

--- env_variables/recipes/default.rb
+++ env_variables/recipes/default.rb
@@ -3,7 +3,11 @@ node[:deploy].each do |app_name, deploy|
     user deploy[:user]
     group deploy[:group]
   
-    path ::File.join(deploy[:deploy_to], "shared")
+    path ::File.join(deploy[:deploy_to], 'shared')
     environment_variables deploy[:environment_variables]
+  end
+  
+  link ::File.join(deploy[:deploy_to], 'shared', 'app.env') do
+    to ::File.join(deploy[:deploy_to], 'current', 'app.env')
   end
 end
