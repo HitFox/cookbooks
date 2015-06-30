e763756756901a3c43ffef1d03c3dfaea67bd301
kr1sp1n <krispinone@googlemail.com>
2015-06-30 15:01:54 +0200

    refactor

--- revive-adserver/recipes/default.rb
+++ revive-adserver/recipes/default.rb
@@ -1,7 +1,17 @@
 
 node[:deploy].each do |application, deploy|
+
+	puts "XXX"
 	puts "#{deploy[:deploy_to]}"
 	puts application
+
+	directory "#{deploy[:deploy_to]}/current/var" do
+  	owner 'root'
+  	group 'root'
+		owner deploy[:user]
+		group deploy[:group]
+  	mode '0777'
+	end
 end
 
 # apt-get install unzip
