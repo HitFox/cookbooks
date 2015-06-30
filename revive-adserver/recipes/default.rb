52b8bc7ace8942921365f507e3c64deebd15bc6e
kr1sp1n <krispinone@googlemail.com>
2015-06-30 14:16:56 +0200

    refactor

--- revive-adserver/recipes/default.rb
+++ revive-adserver/recipes/default.rb
@@ -1,10 +1,17 @@
+
+include_recipe 'deploy'
+
+puts "XXXXX"
+puts deploy[:deploy_to]
+
 bash 'install_adserver' do
   user 'root'
   cwd '/tmp'
   code <<-EOH
-	apt-get install unzip
-  wget http://download.revive-adserver.com/revive-adserver-3.2.1.zip
-	unzip -o revive-adserver-3.2.1.zip
-	ls -lath .
   EOH
 end
+
+# apt-get install unzip
+# wget http://download.revive-adserver.com/revive-adserver-3.2.1.zip
+# unzip -o revive-adserver-3.2.1.zip
+# ls -lath .
