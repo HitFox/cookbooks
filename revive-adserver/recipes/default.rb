f1a25c266850fdfc1d574b7b566e6844aa88193e
kr1sp1n <krispinone@googlemail.com>
2015-07-01 10:36:28 +0200

    refactor

--- revive-adserver/recipes/default.rb
+++ revive-adserver/recipes/default.rb
@@ -7,12 +7,16 @@ node[:deploy].each do |application, deploy|
 			group deploy[:group]
 		  cwd "#{deploy[:deploy_to]}/current"
 		  code <<-EOH
-			chmod -R a+w var
 			mkdir -p var/templates_compiled
+			mkdir -p www/admin/plugins
+
+			chmod -R a+w var
+			chmod -R a+w var/plugins
+			chmod -R a+w var/cache
 			chmod -R a+w var/templates_compiled
 			chmod -R a+w plugins
-			mkdir -p www/admin/plugins
 			chmod -R a+w www/admin/plugins
+			chmod -R a+w www/images
 		  EOH
 		end
 	end
