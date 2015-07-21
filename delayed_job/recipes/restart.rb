3895688d8ac900865583a5fdaa2dbd144bf18d1e
itschn <lofd23@yahoo.de>
2015-07-21 12:00:47 +0200

    Fixing node in restart template

--- delayed_job/recipes/restart.rb
+++ delayed_job/recipes/restart.rb
@@ -1,5 +1,5 @@
 node[:deploy].each do |application, deploy|
-  node[:monit][application][:workers_count].times do |count|
+  node[:delayed_job][application][:workers_count].times do |count|
     Chef::Log.debug("Restarting delayed_job.#{application}.#{count}")
     execute "monit restart delayed_job.#{count}" 
   end
