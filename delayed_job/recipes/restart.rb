89d106608ba708b2c6af4bff8418848134885568
itschn <lofd23@yahoo.de>
2015-07-21 13:15:03 +0200

    Trying defaults to get to work

--- delayed_job/recipes/restart.rb
+++ delayed_job/recipes/restart.rb
@@ -1,5 +1,5 @@
 node[:deploy].each do |application, deploy|
-  node[:delayed_job][application][:workers_count].times do |count|
+  node[:delayed_job][application[:env].to_sym][:workers_count].times do |count|
     Chef::Log.debug("Restarting delayed_job.#{application}.#{count}")
     execute "monit restart delayed_job.#{count}" 
   end
