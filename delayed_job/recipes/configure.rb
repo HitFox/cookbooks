c7c2d8c3b2633a45c864c957daadfad98f19c96c
itschn <lofd23@yahoo.de>
2015-07-21 13:59:47 +0200

    Trying defaults to get to work

--- delayed_job/recipes/configure.rb
+++ delayed_job/recipes/configure.rb
@@ -1,13 +1,6 @@
 include_recipe "deploy"
 
-
-
 node[:deploy].each do |application, deploy|  
-  
-  Chef::Log.info('############################')
-  Chef::Log.info(node[:delayed_job][deploy[:rails_env].to_sym][:workers_count].inspect)
-  Chef::Log.info('############################')
-  
   template "/etc/monit/conf.d/delayed_job.monitrc" do
     source "delayed_job.monit.erb"
     group deploy[:group]
